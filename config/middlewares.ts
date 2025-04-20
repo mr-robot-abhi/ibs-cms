fe171914bc10a4e507dc347c8382f6a524f90041
mr_robot_abhi <your-email@example.com>
2025-04-20 11:09:54 +0100

    Midware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,29 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://your-vercel-app.vercel.app'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://your-vercel-app.vercel.app'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000', 'https://your-vercel-app.vercel.app']
+    }
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
